f3668f808dcd6992f43bed374b0c4e346ee56e75
ReivenIV <runyalen88@duck.com>
2024-12-20 12:41:53 +0200

    lvl1 +1 repeat_alpha

---
+++ rank_2/level_1/repeat_alpha/v1/repeat_alpha.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 10:50:51 by rita              #+#    #+#             */
+/*   Updated: 2024/12/20 12:41:24 by fwebe-ir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int	i;
+	int	multiplier;
+	int	printed_amount;
+
+	// if we have only 2 argument do
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] != '\0')
+		{
+			multiplier = 1;
+			if (argv[1][i] >= 'a' && argv[1][i] <='z')
+				multiplier = argv[1][i] - 'a' + 1;
+			else if (argv[1][i] >= 'A' && argv[1][i] <='Z')
+				multiplier = argv[1][i] - 'A' + 1;
+			printed_amount = 0;
+			while (printed_amount < multiplier)
+			{
+				write(1, &argv[1][i], 1);
+				printed_amount++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
