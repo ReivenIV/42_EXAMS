92a9993e2ab410e0e50a8428eb41107c53f9330b
ReivenIV <runyalen88@duck.com>
2025-01-14 18:16:46 +0200

    print_hex created with references

---
+++ rank_2/level_3/print_hex/v1/print_hex.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_hex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 17:50:47 by rita              #+#    #+#             */
+/*   Updated: 2025/01/14 18:06:01 by rita             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int power;
+	int res;
+
+	power = 1;
+	i = 0;
+	res = 0;
+	if (str[i] == '-')
+	{
+		power = power * -1;
+		i++;
+	}
+	while ((str[i] >= '0' && str[i] <= '9'))
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * power);
+}
+void print_hex(int n)
+{
+	char *digits = "0123456789abcdef";
+
+	if( n > 16)
+		print_hex(n / 16);
+	write(1, &digits[n % 16], 1);
+}
+
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		print_hex(ft_atoi(av[1]));
+	write(1, "\n", 1);
+	return (0);
+}
