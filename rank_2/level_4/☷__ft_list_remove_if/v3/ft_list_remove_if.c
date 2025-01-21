8baadc338f99c30d4413be0a1804b8039c270108
ReivenIV <runyalen88@duck.com>
2025-01-21 12:07:46 +0200

    ft_list_remove_if

--- rank_2/level_4/☷__ft_list_remove_if/v3/ft_list_remove_if.c
+++ rank_2/level_4/☷__ft_list_remove_if/v3/ft_list_remove_if.c
@@ -4,7 +4,11 @@
 /*   Created: 2016/12/05 17:53:59 by angavrel          #+#    #+#             */
 /*   Updated: 2019/03/05 08:39:05 by fwuensche        ###   ########.fr       */
 
-#include "ft_list.h"
+
+// IMPORTANT : i saw these code in several repos. What when i test it i have a bug in the outputs maybe there is something wrong in the main testing area.
+// for a corrected version check : v4 
+
+#include "../ft_list.h"
 #include <stdlib.h>
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
@@ -29,4 +33,63 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 		}
 		curr = curr->next;
 	}
+}
+
+//-------------------------------------------
+// 			:: test it dear !! ::
+//-------------------------------------------
+
+#include <stdio.h>
+#include <string.h>
+void print_list(t_list *list)
+{
+    while (list)
+    {
+        printf("%s -> ", (char *)list->data);
+        list = list->next;
+    }
+    printf("NULL\n");
+}
+
+int cmp(void *data1, void *data2)
+{
+    return strcmp((char *)data1, (char *)data2); // REMINDER:  i'm testing with strings 
+}
+
+t_list *create_elem(void *data)
+{
+    t_list *new_elem = (t_list *)malloc(sizeof(t_list));
+    if (!new_elem)
+        return NULL;
+    new_elem->data = data;
+    new_elem->next = NULL;
+    return new_elem;
+}
+
+int main()
+{
+    t_list *list = create_elem("42");
+    list->next = create_elem("school");
+    list->next->next = create_elem("BUG");
+    list->next->next->next = create_elem("BUG");
+    list->next->next->next->next = create_elem("students");
+    list->next->next->next->next->next = create_elem("BUG");
+
+    printf("Original list:\n");
+    print_list(list);
+
+    ft_list_remove_if(&list, "BUG", cmp);
+
+    printf("Modified list:\n");
+    print_list(list);
+
+    // Free remaining elements
+    while (list)
+    {
+        t_list *tmp = list;
+        list = list->next;
+        free(tmp);
+    }
+
+    return 0;
 }
--- rank_2/level_4/☷__ft_list_remove_if/v4/ft_list_remove_if.c
+++ rank_2/level_4/☷__ft_list_remove_if/v4/ft_list_remove_if.c
@@ -1,33 +1,109 @@
-// source : https://github.com/MPascaud/examshell/blob/master/ft_list_remove_if.c
-/*   By: mpascaud <marvin@42.fr>                    +#+  +:+       +#+        */
-/*   Created: 2018/01/12 15:38:51 by mpascaud          #+#    #+#             */
-/*   Updated: 2018/01/12 15:48:41 by mpascaud         ###   ########.fr       */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 12:06:47 by rita              #+#    #+#             */
+/*   Updated: 2025/01/21 12:07:11 by rita             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+// IMPORTANT : updated version of v3
 
-#include "ft_list.h"
+#include "../ft_list.h"
 #include <stdlib.h>
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list	*tmp;
-	t_list	*i;
+    t_list	*curr;
+    t_list	*tmp;
 
-	while (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
-	{
-		tmp = *begin_list;
-		*begin_list = (*begin_list)->next;
-		free(tmp);
-	}
-	i = *begin_list;
-	while (i && i->next)
-	{
-		if (cmp(i->next->data, data_ref) == 0)
-		{
-			tmp = i->next;
-			i->next = tmp->next;
-			free(tmp);
-		}
-		if (i->next)
-			i = i->next;
-	}
+	if (begin_list == NULL || *begin_list == NULL)
+		return;
+
+    // Remove nodes from the beginning of the list
+    while (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
+    {
+        curr = *begin_list;
+        *begin_list = (*begin_list)->next;
+        free(curr);
+    }
+
+    curr = *begin_list;
+
+    // Traverse the list and remove matching nodes
+    while (curr && curr->next)
+    {
+        if (cmp(curr->next->data, data_ref) == 0)
+        {
+            tmp = curr->next;
+            curr->next = tmp->next;
+            free(tmp);
+        }
+        else
+        {
+            curr = curr->next;
+        }
+    }
+}
+
+//-------------------------------------------
+// 			:: test it dear !! ::
+//-------------------------------------------
+
+#include <stdio.h>
+#include <string.h>
+void print_list(t_list *list)
+{
+    while (list)
+    {
+        printf("%s -> ", (char *)list->data);
+        list = list->next;
+    }
+    printf("NULL\n");
+}
+
+int cmp(void *data1, void *data2)
+{
+    return strcmp((char *)data1, (char *)data2); // REMINDER:  i'm testing with strings 
+}
+
+t_list *create_elem(void *data)
+{
+    t_list *new_elem = (t_list *)malloc(sizeof(t_list));
+    if (!new_elem)
+        return NULL;
+    new_elem->data = data;
+    new_elem->next = NULL;
+    return new_elem;
+}
+
+int main()
+{
+    t_list *list = create_elem("42");
+    list->next = create_elem("school");
+    list->next->next = create_elem("BUG");
+    list->next->next->next = create_elem("BUG");
+    list->next->next->next->next = create_elem("students");
+    list->next->next->next->next->next = create_elem("BUG");
+
+    printf("Original list:\n");
+    print_list(list);
+
+    ft_list_remove_if(&list, "BUG", cmp);
+
+    printf("Modified list:\n");
+    print_list(list);
+
+    // Free remaining elements
+    while (list)
+    {
+        t_list *tmp = list;
+        list = list->next;
+        free(tmp);
+    }
+
+    return 0;
 }
