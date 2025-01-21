f1219d344fdbda2a23eb19e5b3aecd4f65e9a7fb
ReivenIV <runyalen88@duck.com>
2025-01-21 19:53:20 +0200

    wdmatch comments created

--- rank_2/level_2/◣__ wdmatch/v2_♡/wdmatch.c
+++ rank_2/level_2/◣__ wdmatch/v2_♡/wdmatch.c
@@ -11,22 +11,22 @@ int main(int ac, char **av)
     {
         j = 0;
         i = 0;
-        while (av[2][j])
+        while (av[2][j])                    // will loop until the end of av[2]
         {
             if (av[1][i] == av[2][j])
-                i++;
+                i++;                        // if they are the same, av[1][i++]
             j++;
         }
-        if (av[1][i] == '\0')
+        if (av[1][i] == '\0')               // when the loop finished on av[2] & if av[1] reached the end. 
         {
             i = 0;
             while(av[1][i])
             {
-                write(1, &av[1][i], 1);
+                write(1, &av[1][i], 1);     // We print it all. 
                 i++;
             }
         }
     }
-    write(1, "\n", 1);
+    write(1, "\n", 1);                      // don't forget the \n
     return(0);
 }
