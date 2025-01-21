de4a972fe701516a8fb7e35ebe706734dd02cfb2
ReivenIV <runyalen88@duck.com>
2025-01-21 19:09:37 +0200

    ulstr updated with favorite

--- rank_2/level_1/ulstr/v1/ulstr.c
+++ rank_2/level_1/ulstr/v1/ulstr.c
@@ -1,32 +1,20 @@
 //source : unknown but i saw it everywhere. 
 
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ulstr.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/20 15:21:54 by rita              #+#    #+#             */
-/*   Updated: 2024/12/20 15:21:54 by rita             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-int	main(int argc, char **argv)
+int	main(int ac, char **av)
 {
 	int	i;
 
-	if (argc == 2)
+	if (ac == 2)
 	{
 		i = 0;
-		while (argv[1][i] != '\0')
+		while (av[1][i] != '\0')
 		{
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				argv[1][i] = argv[1][i] - 32;
-			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i] = argv[1][i] + 32;
-			write(1, &argv[1][i], 1);
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+				av[1][i] = av[1][i] - 32;					// a-A=32 -> 'a'+32= 'A'
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				av[1][i] = av[1][i] + 32;
+			write(1, &av[1][i], 1);
 			i++;
 		}
 	}
