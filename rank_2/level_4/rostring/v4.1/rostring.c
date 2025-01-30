7d7ecab7ac68c38ca9bf12fb8a9e27091b68d89a
ReivenIV <runyalen88@duck.com>
2025-01-30 12:07:15 +0200

    rostring

---
+++ rank_2/level_4/rostring/v4.1/rostring.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+    int	i = 0;
+    int	space = 0;
+    int	start;
+    int	end;
+
+    if (ac > 1 && av[1][0]) 
+    {	
+        while (av[1][i] == ' ' || av[1][i] == '\t')
+            i++;
+        start = i;
+        while (av[1][i] != ' ' && av[1][i] != '\t' && av[1][i])												// first word founded
+            i++;
+        end = i;
+        while (av[1][i] == ' ' || av[1][i] == '\t')
+            i++;
+        while (av[1][i])
+        {
+            while ((av[1][i] == ' ' && av[1][i + 1] == ' ') || (av[1][i] == '\t' && av[1][i + 1] == '\t')) // Skip multiple spaces or tabs
+                i++;
+            if (av[1][i] == ' ' || av[1][i] == '\t') 														// Set space flag if a space or tab is found
+                space = 1;
+            write(1, &av[1][i], 1); 																		// Print the current character
+            i++;
+        }
+        if (space) 																							// Print a space before the first word if there were other words
+            write(1, " ", 1);
+        while (start < end)																					// Print the first word at the end
+        {
+            write(1, &av[1][start], 1);
+            start++;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
