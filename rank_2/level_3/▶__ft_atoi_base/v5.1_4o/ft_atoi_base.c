284a517ec3fa9a7fe92536b5160a89ba2c375a10
ReivenIV <runyalen88@duck.com>
2025-02-05 17:08:17 +0200

    atoi base v5_4o v5.1_4o created

--- rank_2/level_3/▶__ft_atoi_base/v5.1_4o/ft_atoi_base.c
+++ rank_2/level_3/▶__ft_atoi_base/v5.1_4o/ft_atoi_base.c
@@ -6,51 +6,95 @@
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:12:23 by rita              #+#    #+#             */
-/*   Updated: 2025/02/05 09:32:01 by rita             ###   ########.fr       */
+/*   Updated: 2025/02/05 17:07:47 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <ctype.h>
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
 
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+    return (0);
+}
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
 int ft_atoi_base(const char *str, int str_base) {
+    int i = 0;
     int result = 0;
     int sign = 1;
     int digit;
 
     // Handle empty string
-    if (!str || *str == '\0')
-        return 0;
+    if (!str || str[i] == '\0')
+        return (0);
 
     // Handle negative sign
-    if (*str == '-') {
+    if (str[i] == '-')
+    {
         sign = -1;
-        str++;
+        i++;
     }
 
     // Convert string to integer
-    while (*str) {
-        if (isdigit(*str))
-            digit = *str - '0';
-        else if (isalpha(*str))
-            digit = tolower(*str) - 'a' + 10;
+    while (str[i])
+    {
+        if (ft_isdigit(str[i]) == 1)
+            digit = str[i] - '0';
+        else if (ft_isalpha(str[i]) == 1)
+            digit = ft_tolower(str[i]) - 'a' + 10;
         else
             break;
 
-        if (digit >= str_base)
+        if (digit >= str_base)                          // If the digit is not valid in the given base, exit the loop
             break;
 
-        result = result * str_base + digit;
-        str++;
+        result = result * str_base + digit;             // This line updates the result by multiplying the current result by the base (str_base) and then adding the current digit. This effectively shifts the current result left by one digit in the given base and adds the new digit to the least significant place.
+        i++;
     }
-
     return result * sign;
 }
+
+//   ----------------------
+//   ::  visualisation   :: 
+//   ----------------------
+/*
+    Assume we have the string "1a" and the base is 16 (hexadecimal).
+
+    ## First Iteration:
+
+    - str[i] is '1'.
+    - ft_isdigit('1') returns 1, so digit = '1' - '0' which is 1.
+    - result = result * 16 + digit which is 0 * 16 + 1 resulting in 1.
+   
+    ## Second Iteration:
+
+    - str[i] is 'a'.
+    - ft_isdigit('a') returns 0, so we check ft_isalpha('a') which returns 1.
+    - digit = ft_tolower('a') - 'a' + 10:
+    - ft_tolower('a') is 'a'.
+    - 'a' - 'a' is 0.
+    - 0 + 10 is 10.
+    - result = result * 16 + digit which is 1 * 16 + 10 resulting in 26.
+    
+    >>> So, the string "1a" in base 16 is converted to the integer 26. <<<
+*/
 
 
 //   ---------------------
 //   ::  test it dear   :: 
 //   ---------------------
+#include <stdio.h>
 // test XS (handy for the exam)
 // // int main() {
 // //     const char *number = "F4240";
